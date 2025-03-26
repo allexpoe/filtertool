6b50e8b3f6ef2eb60639bd81089505a9433eb52e
allex <allexpleblord@gmail.com>
2025-03-26 23:16:57 +0200

    Fix some issues after updating with autogenerated types from main repo

--- src/scripts/createTTS.ts
+++ src/scripts/createTTS.ts
@@ -1,35 +1,33 @@
 import axios from 'axios'
+import { exec } from 'child_process'
 import fs from 'fs'
 import path from 'path'
-import { exec } from 'child_process'
 
 export async function createTTSFile(filename: string): Promise<void> {
-    if (fs.existsSync(filename)) {
-        return
+  if (fs.existsSync(filename)) {
+    return
+  }
+
+  const text = path.basename(filename, path.extname(filename))
+  const url = `https://api.streamelements.com/kappa/v2/speech?voice=Brian&text=${encodeURIComponent(text)}`
+  const basename = path.basename(filename)
+  try {
+    const response = await axios.get<ArrayBuffer>(url, { responseType: 'arraybuffer' })
+    fs.writeFileSync(basename, Buffer.from(response.data))
+
+    if (!fs.existsSync(path.dirname(filename))) {
+      fs.mkdirSync(path.dirname(filename), { recursive: true })
     }
 
-    const text = path.basename(filename, path.extname(filename));
-    const url = `https://api.streamelements.com/kappa/v2/speech?voice=Brian&text=${encodeURIComponent(text)}`;
-    const basename = path.basename(filename);
-    try {
-        const response = await axios.get<ArrayBuffer>(url, { responseType: 'arraybuffer' });
-        fs.writeFileSync(basename, Buffer.from(response.data))
-
-        if (!fs.existsSync(path.dirname(filename))) {
-            fs.mkdirSync(path.dirname(filename), { recursive: true });
-        }
-
-        exec(`ffmpeg -y -i ${basename} -af "volume=3.5, atempo=1.5" -write_xing 0 -loglevel quiet "${filename}"`,
-            (err) => {
-                if (err) {
-                    console.error(`Error processing ${filename}:`, err);
-                } else {
-                    fs.unlinkSync(basename); // Remove the original file
-                    console.log(`Created ${filename}`);
-                }
-            }
-        );
-    } catch (error) {
-        console.error(`Error while creating ${filename}:`, error);
-    }
+    exec(`ffmpeg -y -i ${basename} -af "volume=3.5, atempo=1.5" -write_xing 0 -loglevel quiet "${filename}"`, (err) => {
+      if (err) {
+        console.error(`Error processing ${filename}:`, err)
+      } else {
+        fs.unlinkSync(basename) // Remove the original file
+        console.log(`Created ${filename}`)
+      }
+    })
+  } catch (error) {
+    console.error(`Error while creating ${filename}:`, error)
+  }
 }
